ae27e2e6f6687a096574cc1ad0923277501254c3
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-04-15 23:16:49 -0400

    Done database.py + working on models.py

--- app/database.py
+++ app/database.py
@@ -1,2 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgresql"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, Float
+from .database import Base
+
+class Listing(Base):
+  __tablename__ = "listings"
+
+  id = Column(Integer, primary_key=True, index=True)
+  title = Column(String, index=True)
+  description = Column(Float)
