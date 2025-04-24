5ea0d79894269421743b9bb28e94cb480b188618
beulahpriscilla <beulahpriscilla26@gmail.com>
2025-04-24 17:38:33 -0400

    added get_db() to database.py

--- app/database.py
+++ app/database.py
@@ -7,3 +7,10 @@ DATABASE_URL = "postgresql://postgres:password@localhost:5432/postgresql"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
